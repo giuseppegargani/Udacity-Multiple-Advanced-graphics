0666322fdfe62bfc41f16473ce3cd8ee6506370d
Houde Giuseppe Gargani <sentierodisole1@yahoo.it>
2021-06-25 11:57:30 +0200

    Commit Iniziale

--- app/src/main/java/com/example/android/customfancontroller/MainActivity.kt
+++ app/src/main/java/com/example/android/customfancontroller/MainActivity.kt
@@ -2,41 +2,66 @@ package com.example.android.customfancontroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//TODO 1 - Costruisci una classe ("DialView") che estende View e di conseguenza importa l'elemento View
-//TODO 2 - Aggiungi il costruttore della View usando '@JvmOverloads'
-//Approfondimento qui: https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.jvm/-jvm-overloads/
-//Altro approfondimento: https://developer.android.com/training/custom-views/index.html
-//TODO 3 - Aggiungi un Enum Class (subito sotto le importazioni) e come una altra classe
+
+//TODO - 1 Override del metodo OnSizeChanged
 /*
-private enum class FanSpeed(val label: Int) {
-   OFF(R.string.fan_off),
-   LOW(R.string.fan_low),
-   MEDIUM(R.string.fan_medium),
-   HIGH(R.string.fan_high);
+radius = (min(width, height) / 2.0 * 0.8).toFloat()
+ */
+//TODO - 2 Aggiungi la seguente funzione che compie dei calcoli
+/*
+private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
+   // Angles are in radians.
+   val startAngle = Math.PI * (9 / 8.0)
+   val angle = startAngle + pos.ordinal * (Math.PI / 4)
+   x = (radius * cos(angle)).toFloat() + width / 2
+   y = (radius * sin(angle)).toFloat() + height / 2
 }
  */
-//TODO 4 - Sotto Enum Aggiungi queste costanti:
+//TODO - 3 Fai override del metodo onDraw per disegnare            super.onDraw(canvas)
+//TODO - 4 Dentro il metodo onDraw aggiungi questa riga per modificare il colore sulla base dei valori  (DOPO super)!!!!!!
 /*
-private const val RADIUS_OFFSET_LABEL = 30
-private const val RADIUS_OFFSET_INDICATOR = -35
- */
-//TODO 5 - Dentro DialView Class importa queste costanti
-/*
-private var radius = 0.0f                   // Radius of the circle.
-private var fanSpeed = FanSpeed.OFF  //The active selection.
-//position variable which will be used to draw label and indicator circle position
-private val pointPosition: PointF = PointF(0.0f, 0.0f)
+// Set dial background color to green if selection not off.
+paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
  */
 
-//TODO 6 - DENTRO DIALVIEW INIZIALIZZA UN OGGETTO PAINT (con importazioni)
+//TODO - 5 Disegna un cerchio (dentro onDraw)
 /*
-private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-   style = Paint.Style.FILL
-   textAlign = Paint.Align.CENTER
-   textSize = 55.0f
-   typeface = Typeface.create( "", Typeface.BOLD)
+// Draw the dial.
+canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
+ */
+//TODO - 6 DISEGNA (dentro onDraw) un cerchio più piccolo
+/*
+// Draw the indicator circle.
+val markerRadius = radius + RADIUS_OFFSET_INDICATOR
+pointPosition.computeXYForSpeed(fanSpeed, markerRadius)
+paint.color = Color.BLACK
+canvas.drawCircle(pointPosition.x, pointPosition.y, radius/12, paint)
+ */
+//TODO - 7 Disegna le annotazioni
+/*
+// Draw the text labels.
+val labelRadius = radius + RADIUS_OFFSET_LABEL
+for (i in FanSpeed.values()) {
+   pointPosition.computeXYForSpeed(i, labelRadius)
+   val label = resources.getString(i.label)
+   canvas.drawText(label, pointPosition.x, pointPosition.y, paint)
 }
  */
+//TODO - 8 in Activity_main.xml  sostituisci ImageView con DialView e cancella attributo android:background
+//TODO - 9 DIALVIEW ha le seguenti impostazioni:
+/*
+<com.example.android.customfancontroller.DialView
+       android:id="@+id/dialView"
+       android:layout_width="@dimen/fan_dimen"
+       android:layout_height="@dimen/fan_dimen"
+       app:layout_constraintTop_toBottomOf="@+id/customViewLabel"
+       app:layout_constraintLeft_toLeftOf="parent"
+       app:layout_constraintRight_toRightOf="parent"
+       android:layout_marginLeft="@dimen/default_margin"
+       android:layout_marginRight="@dimen/default_margin"
+       android:layout_marginTop="@dimen/default_margin" />
+ */
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
